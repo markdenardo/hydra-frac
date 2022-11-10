6dc5e50935ad3bd9dcacd4298427a52bd85b834c
MDN <mark.denardo@gmail.com>
2022-11-09 20:18:12 -0500

    Create frac_01.js

---
+++ frac_01.js
@@ -0,0 +1,44 @@
+// licensed with CC BY-NC-SA 4.0 https://creativecommons.org/licenses/by-nc-sa/4.0/
+// sierpinski triangle made with only feedback
+// ritchse aka geisha
+
+await import("https://hydra-extensions.glitch.me/hydra-nowrap.js")
+await import("https://hydra-extensions.glitch.me/hydra-outputs.js")
+await import("https://hydra-extensions.glitch.me/hydra-canvas.js")
+await import("https://hydra-extensions.glitch.me/hydra-mouse.js")
+await import("https://cdn.statically.io/gl/metagrowing/extra-shaders-for-hydra/main/lib/lib-noise.js")
+
+setResolution(720,720)
+oS.setNearest()
+oS.setClamp()
+canvas.setRelativeSize(1)
+canvas.setLinear()
+canvas.setAlign('center')
+
+setFunction({
+	name: 'triangle',
+    type: 'src',
+  	inputs: [],
+    glsl: `
+    	float l = abs(0.5-_st.x);
+        float c = l <= (_st.y*0.5) ? 1.0 : 0.0;
+        c *= _st.y <= 1.0 ? 1.0 : 0.0; // feisimo esto
+        return vec4(c);
+    `
+})
+
+modtoggle = 1
+window.onclick = ()=> modtoggle ^= 1
+
+input = ()=> src(o0).mask(triangle())
+feedback = ()=> input().scale(.5).modulateScale(input(),()=>modtoggle*.2)
+dist = ()=> Math.hypot(mouse.cposx,mouse.cposy)/2
+ndist = ()=> 0-dist()
+solid(0,0,0,0)
+	.layer(feedback().scroll(dist,ndist))
+	.layer(feedback().scroll(ndist,ndist))
+	.layer(feedback().scroll(0,dist))
+	.add(colornoise(1,.2),.23)
+	.hue(.04).color(1.02,.84,.98).saturate(1.5)
+	.blend(src(o0).scale(1.005),.75)
+	.out()
